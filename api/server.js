608511554c4582b23b8f737b19a88838a692abf9
Zac Smith <zrs3141592@gmail.com>
2019-08-24 13:04:50 -0600

    scaffold out API

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+const logger = require("morgan");
+
+const carsRouter = require("../cars/cars-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(logger("dev"));
+server.use("/api/cars", carsRouter);
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,4 @@
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 4003;
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
