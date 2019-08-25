ee5fbe633ce2207b5c3ccdfa910cd11b3826c426
Zac Smith <zrs3141592@gmail.com>
2019-08-25 17:48:49 -0600

    create GET, GET:id, POST operations

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -11,6 +11,51 @@ const db = knex({
 
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+// router.get("/", (req, res) => {
+//   db("cars")
+//     .then(car => {
+//       res.status(200).json(car);
+//     })
+//     .catch(error => {
+//       res.status(500).json({
+//         message: "Unable to GET cars"
+//       });
+//     });
+// });
+
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
+//   db("cars")
+//     .where({ id })
+//     .first()
+//     .then(car => {
+//       res.status(200).json(car);
+//     })
+//     .catch(error => {
+//       res.status(500).json({
+//         message: "Unable to GET your car by id"
+//       });
+//     });
+// });
+
+// router.post("/", (req, res) => {
+//   const carData = req.body;
+
+//   db("cars")
+//     .insert(carData)
+//     .then(ids => {
+//       db("cars")
+//         .where({ id: ids[0] })
+//         .then(newCar => {
+//           res.status(200).json({ newCar });
+//         });
+//     })
+//     .catch(error => {
+//       console.log("POST error", error);
+//       res.status(500).json({
+//         message: "Unable to POST your car data"
+//       });
+//     });
+// });
 
 module.exports = router;
