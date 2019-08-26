7b0f8867a65d09fbf21895fce905684ccb6b6696
Zac Smith <zrs3141592@gmail.com>
2019-08-25 19:58:03 -0600

    fixed schema and seeds

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 4003;
+const port = process.env.PORT || 4004;
 server.listen(port, () => console.log(`Server is listening on port ${port}`));
